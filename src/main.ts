902fdcad474a5ff9e62cd1c347ca82fafa2c15be
Shpak Alexandr Sergeevich <shpak.alexandr.sergeevich@yandex.ru>
2023-08-22 12:22:20 +0300

    [#5] Добавлено использование dotenv
     - добавлена зависимость dotenv;
     - добавлены переменнрые окружения PORT и HOST
     - добавлено исопльзование переменных окружения

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SWAGGER_CONSTANTS } from './constants/swagger/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, documentBuilder);
   SwaggerModule.setup(SWAGGER_CONSTANTS.PATH, app, document);
 
-  await app.listen(3000);
+  await app
+    .listen(+process.env.PORT, process.env.HOST)
+    .then(() => {
+      console.log(
+        `Server started on ${process.env.PORT} port with host name "${process.env.HOST}"!`,
+      );
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
 }
 
 bootstrap();

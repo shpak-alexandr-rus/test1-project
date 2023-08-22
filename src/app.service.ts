4941763b65e9237dd41baa05a09d4fd5f51b12bb
Shpak Alexandr Sergeevich <shpak.alexandr.sergeevich@yandex.ru>
2023-08-22 11:00:38 +0300

    [#1] Подключен swagger
    Подключена генерация документов при помощи swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<h1>Добро пожаловать!</h1>
-    <h1>Это проект Test1 Project.</h1>`;
+    return `<h1 align="center">Добро пожаловать!</h1>
+    <h1 align="center">Это проект Test1 Project.</h1>`;
   }
 }
---
+++ src/constants/swagger/swagger.ts
@@ -0,0 +1,6 @@
+export enum SWAGGER_CONSTANTS {
+  TITLE = 'Test1 Project',
+  DESCRIPTION = 'Test1 Project swagger documentation',
+  VERSION = '1.0',
+  PATH = 'swagger',
+}
